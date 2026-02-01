4ff45ed326128c13a3546ebd3ec78cea7f232942
shabnoor37 <shabnoor.ahmed.37@gmail.com>
2026-02-02 00:45:05 +0530

    Create test cases for calculator functions
    Add unit tests for add and substract functions

---
+++ test/test_calculator.py
@@ -0,0 +1,11 @@
+from app.calculator import add,substract  
+
+'''
+from app -> folder
+calculator add, substract (functions)
+'''
+def test_add():
+  assert add(2,3) == 5
+
+def test_substract():
+  assert substract(5,3) == 2
